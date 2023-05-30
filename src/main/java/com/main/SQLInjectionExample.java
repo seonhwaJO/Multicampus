e5826be5d5d3a4d7675053f833d5188b625235ef
SeonhwaJO <chekhov12@naver.com>
2023-05-30 14:35:42 +0900

    정적 분석2

--- src/main/java/com/main/SQLInjectionExample.java
+++ src/main/java/com/main/SQLInjectionExample.java
@@ -1,15 +1,14 @@
 package com.main;
 
 public class SQLInjectionExample {
-	public static void main(String[] args) {
-		String username = getUserInput();
-		String query = "select*from user where username = '"+username+"'";
-		//쿼리 실행 로직
-		System.out.println(query);
-	}
+    public static void main(String[] args) {
+        String username = getUserInput();
+        String query = "SELECT * FROM users WHERE username = '" + username + "'";
+        // query 실행 로직
+    }
 
-	private static String getUserInput() {
-		// 사용자로 입력 받은 로직
-		return "TEST" ;
-	}
+    private static String getUserInput() {
+        // 사용자로부터 입력 받는 로직
+        return "TEST";
+    }
 }
