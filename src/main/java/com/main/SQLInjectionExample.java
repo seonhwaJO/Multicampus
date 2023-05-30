8298d187b73316b891d4399c714a652f4bab598d
SeonhwaJO <chekhov12@naver.com>
2023-05-30 14:20:40 +0900

    sql 인젝션 테스트

---
+++ src/main/java/com/main/SQLInjectionExample.java
@@ -0,0 +1,15 @@
+package com.main;
+
+public class SQLInjectionExample {
+	public static void main(String[] args) {
+		String username = getUserInput();
+		String query = "select*from user where username = '"+username+"'";
+		//쿼리 실행 로직
+		System.out.println(query);
+	}
+
+	private static String getUserInput() {
+		// 사용자로 입력 받은 로직
+		return "admin' --" ;
+	}
+}
